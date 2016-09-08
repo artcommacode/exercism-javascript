113a62570e7d97413714fd37ee66989c6f70f704
Ryan <ryan@superpositionkitty.com>
2016-09-08 14:57:05 +0100

    triangle

---
+++ triangle/triangle.js
@@ -0,0 +1,31 @@
+module.exports = class {
+  constructor(...args) {
+    this.triangle = args.sort((x, y) => x - y)
+  }
+
+  sidesSame() {
+    return this.triangle.reduce((acc, side, i) => {
+      return acc + (this.triangle.indexOf(side) !== i ? 1 : 0)
+    }, 1)
+  }
+
+  hasBadLength() {
+    return this.triangle.reduce((acc, side) => side <= 0 ? true : acc, false)
+  }
+
+  violatesEquality() {
+    return this.triangle[0] + this.triangle[1] < this.triangle[2]
+  }
+
+  kind() {
+    if (this.hasBadLength() || this.violatesEquality()) throw Error()
+    switch (this.sidesSame()) {
+      case 3:
+        return 'equilateral'
+      case 2:
+        return 'isosceles'
+      default:
+        return 'scalene'
+    }
+  }
+}
